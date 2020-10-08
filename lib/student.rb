09859560ad87e829fab034481b90e30cdae8d04c
Michael Ballard <ballard.michael9@gmail.com>
2020-10-08 05:15:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -69,6 +69,17 @@ class Student
     end
   end
 
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+        SELECT * FROM students
+        WHERE grade = 10
+        LIMIT 1
+    SQL
+    DB[:conn].execute(sql, x).map do |row|
+        self.new_from_db(row)
+    end.first
+  end
+
 
   def save
     sql = <<-SQL
