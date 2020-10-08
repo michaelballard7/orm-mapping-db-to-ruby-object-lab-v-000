2f2f1a6d576ce902b50635f9cde8056c8b8bc83f
Michael Ballard <ballard.michael9@gmail.com>
2020-10-08 05:00:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,7 @@ class Student
   def self.all_students_in_grade_9(grade)
     sql = <<-SQL
         SELECT * FROM students
-        WHERE grae = ?
+        WHERE grade = ?
     SQL
     DB[:conn].execute(sql, grade).map do |row|
         self.new_from_db(row)
